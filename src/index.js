7b4cc88e1d6be860eaa51a674e963582ba29f207
dudi709 <dudi709@gmail.com>
2021-03-06 01:44:10 +0200

    Add homepage and loginpage files with design

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ const express  = require('express')
 const app_port = process.env.PORT || 3000
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('hello world555')
-})
+app.get('/home', function(req,res){
+    res.send(__dirname + '/src/home.html');
+}); 
 
 app.listen(app_port)
 console.log(`app is running. port: ${app_port}`)
