87917fc43915563e0299abec416a83b639a883cc
dudi709 <dudi709@gmail.com>
2021-03-06 11:53:52 +0200

    routing the html files

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
-const express  = require('express')
+const express = require('express')
 const app_port = process.env.PORT || 3000
 const app = express()
+const path = require('path')
+const router = express.Router()
 
-app.get('/', function(req,res){
-    res.sendFile(__dirname + '\\home.html')
+app.use(express.static("public"))
+
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname, '../public/html/home.html'))
 })
 
-app.get('/login', function(req,res){
-    res.sendFile(__dirname + '\\login.html')
+router.get('/login',function(req,res){
+  res.sendFile(path.join(__dirname, '../public/html/login.html'))
 })
 
+app.use('/css',express.static(__dirname +'/css'));
+
+//add the router
+app.use('/', router)
 app.listen(app_port)
+
 console.log(`app is running. port: ${app_port}`)
 console.log(`http://127.0.0.1:${app_port}/`)
