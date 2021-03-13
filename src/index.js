8ff1c0465755a9e58ab94f1d9a6ad7a22e43543d
avihay <avihaymaman1234@gmail.com>
2021-03-13 12:38:48 +0200

    test

--- src/index.js
+++ src/index.js
@@ -7,17 +7,12 @@ const MongoClient = require('mongodb').MongoClient
 const connectionString = 'mongodb+srv://team15:Ade123321!@cluster0.3jopa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
 
-
-
-
-
-
 MongoClient.connect(connectionString, { 
   useUnifiedTopology: true })
   .then(client => {
     console.log('Connected to Database')
     const db = client.db('MedicalDB')
-    const quotesCollection = db.collection('quotes')
+    const usersCollection = db.collection('users')
     app.set('view engine', 'ejs')
 
     app.use(express.static('public'))
@@ -37,20 +32,18 @@ MongoClient.connect(connectionString, {
     })
     
     router.post('/signup', (req, res) => {
-      quotesCollection.insertOne(req.body)
+      usersCollection.insertOne(req.body)
         .then(result => {
           res.redirect('/')
         })
         .catch(error => console.error(error))
     })
     app.use('/', router)//add the router
-    module.exports = app.listen(app_port)
+    
     
   })
   .catch(error => console.error(error))
 
-
-
-
+module.exports = app.listen(app_port)
 console.log(`app is running. port: ${app_port}`)
 console.log(`http://127.0.0.1:${app_port}/`)
